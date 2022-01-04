9d63bf71faa7e72481e12ee890f4dc8d7085008c
Andrea Cioccarelli <andrea.cioccarelli01@gmail.com>
2022-01-04 14:33:32 +0100

    Polish

--- core/logic/src/main/kotlin/com/cioccarellia/checkpigeon/ext/EngineExts.kt
+++ core/logic/src/main/kotlin/com/cioccarellia/checkpigeon/ext/EngineExts.kt
@@ -4,11 +4,7 @@ import com.cioccarellia.checkpigeon.logic.model.player.Player
 import com.cioccarellia.checkpigeon.logic.model.tile.TileColor
 
 fun Pair<Player, Player>.findPlayerWithColor(color: TileColor) = when (color) {
-    first.color -> {
-        first
-    }
-    second.color -> {
-        second
-    }
+    first.color -> first
+    second.color -> second
     else -> throw IllegalArgumentException("Data inconsistency in $this: no player colored $color")
 }
